7bdd098abf7c21f5f48292352256cbed61244677
Mike Sanders <71601403+Mike-C-Sanders@users.noreply.github.com>
2020-11-14 21:15:15 -0800

    Updated OrganizePhotos.py Code
    Added a make directory function
    
    Added two loops. One loop to create a list of place names, which are added to a list. This list is passed to the create directory function. 
    
    The second loop is using the originals list (list of all photos), and then adding each photo in the list to the new directory accordingly.

--- OrganizePhotos.py
+++ OrganizePhotos.py
@@ -1,12 +1,34 @@
 import os
 
-os.chdir("Photos")
-originals = os.listdir()
-
-print(originals)#printing to test my code. 
-
-#My Second atempt using the find method
+#A simpler way to write the extract place function
 def extract_place(filename):
-    parts = filename.split("_") # Get a list containing all the parts
-    place_name = parts[1] # Use the index operator to select the second list item
-    return place_name"""
+    return filename.split('_')[1]
+
+#Create a directory for each location
+def make_place_directories(places):
+    for place in places:
+        os.mkdir(place)
+
+os.chdir("Photos")
+
+originals = os.listdir()
+places = []
+
+#This loop creates the list needed to pass into the create directory function
+for filename in originals:
+    place = extract_place(filename)
+    #To ensure we don't pass make_place function duplicates add 
+    # an if statement before appending
+    if place not in places:
+        places.append(place)
+
+
+#Calling make directory and passing the recently appended place function
+make_place_directories(places)
+
+#loop designed to move the files into the directory 
+for filename in originals:
+    place = extract_place(filename)
+    os.rename(filename, os.path.join(place, filename))
+    
+print(os.listdir())#way to check my work
