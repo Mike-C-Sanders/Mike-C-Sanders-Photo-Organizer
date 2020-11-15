16a5415871699db35087d0bbae4cfe804e1c64d0
Mike Sanders <71601403+Mike-C-Sanders@users.noreply.github.com>
2020-11-14 21:25:21 -0800

    Updated OrganizePhotos - Any Directory
    Added a function to allow a user to use this code using any directory, not just the directory name on my computer, which happens to be "Photos".

--- OrganizePhotos.py
+++ OrganizePhotos.py
@@ -1,5 +1,6 @@
 import os
 
+
 #A simpler way to write the extract place function
 def extract_place(filename):
     return filename.split('_')[1]
@@ -9,26 +10,29 @@ def make_place_directories(places):
     for place in places:
         os.mkdir(place)
 
-os.chdir("Photos")
+def organize_photos(directory):
+    #First, extract the place names.
+    os.chdir(directory)
+    originals = os.listdir()
+    places = []
+    #This loop creates the list needed to pass into the create directory function
+    for filename in originals:
+        place = extract_place(filename)
+        #To ensure we don't pass make_place function duplicates add 
+        # an if statement before appending
+        if place not in places:
+            places.append(place)
+            
+    #Second, make place directories.
+    
+    make_place_directories(places)
 
-originals = os.listdir()
-places = []
+    #Third, move files to the directory.
+    for filename in originals:
+        place = extract_place(filename)
+        os.rename(filename, os.path.join(place, filename))
 
-#This loop creates the list needed to pass into the create directory function
-for filename in originals:
-    place = extract_place(filename)
-    #To ensure we don't pass make_place function duplicates add 
-    # an if statement before appending
-    if place not in places:
-        places.append(place)
-
-
-#Calling make directory and passing the recently appended place function
-make_place_directories(places)
-
-#loop designed to move the files into the directory 
-for filename in originals:
-    place = extract_place(filename)
-    os.rename(filename, os.path.join(place, filename))
+organize_photos("Photos")
     
 print(os.listdir())#way to check my work
+
